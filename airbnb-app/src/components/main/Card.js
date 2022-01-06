01c369d02e43aefe3b6d12e95e7404a2064a359f
vivian <vivian.v18723@gmail.com>
2022-01-06 18:58:51 +0000

    Create card component and add image.

---
+++ airbnb-app/src/components/main/Card.js
@@ -0,0 +1,26 @@
+import swimmer from "../images/swimmer.png";
+
+const Card = () => {
+  return (
+    <section className="main2">
+      <figure>
+        <img src={swimmer} alt={"Swimmer called katie Zaferes."} />
+      </figure>
+      <p>SOLD OUT</p>
+      <ul className="bullet-points">
+        <li>
+          <span>
+            <img src={"../images/star.png"} />
+          </span>
+          5.0
+          <span className="country"> (6) USA </span>
+        </li>
+        <li>Life lessons with Katie Zaferes</li>
+        <li>
+          <b>From $136</b> / person
+        </li>
+      </ul>
+    </section>
+  );
+};
+export default Card;
